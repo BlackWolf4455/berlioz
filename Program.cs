edb0afd67c2a32d62ec2c8b33b339457c842789d
BlackWolf4455 <90320042+BlackWolf4455@users.noreply.github.com>
2021-12-28 10:45:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Random rnd = new Random();
+double[] mas = new double[4];
+double max=0;
+double min = 10000;
+for (int i = 0; i< mas.Length;i++)
+{
+    mas[i] = rnd.Next(-10,10);
+    Console.Write("{0}, ", mas[i]);
+    if(max<mas[i]) max = mas[i];
+}
+   Console.WriteLine("max={0}, ", max);
